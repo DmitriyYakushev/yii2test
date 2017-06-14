76820faa4bf7061f2ff57b1fc8af445d40a143a0
Darivush <admin@pinxterapp.com>
2017-06-14 17:36:17 +0300

    add default config for db

---
+++ common/config/db_local.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=cmo-template',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+];
